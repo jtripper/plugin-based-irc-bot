baf8cb581486a917ca4d0f0b3ee3ede28c5f8f04
jtripper <jack@jtripper.net>
2013-04-07 12:50:55 +0000

    added async sockets

--- plugins/twitter.py
+++ plugins/twitter.py
@@ -21,20 +21,20 @@ import HTMLParser
 import config
 
 class twitter:
-  def __init__(self, bot):
+  def __init__(self):
     self.allowed_functions = { 'tweet':1, 'follow':2, 'reply':1, 'retweet':1, 'delete':2, 'help':1 }
 
     auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
     auth.set_access_token(config.access_token, config.access_token_secret)
     self.api = tweepy.API(auth)
-    self.bot = bot
+    
 
-  def tweet(self, buffer):
+  def tweet(self, bot, sock, buffer):
     text       = ' '.join(buffer.msg.split()[1:])
 
     if len(text) < 140:
       tweet = self.api.update_status(text)
-      self.bot.msg(buffer.to, "Tweet URL: https://twitter.com/%s/status/%s" % (tweet.author.screen_name, tweet.id))
+      sock.msg(buffer.to, "Tweet URL: https://twitter.com/%s/status/%s" % (tweet.author.screen_name, tweet.id))
 
     else:
       split_msg = list(re.findall(".?"*110, text))
@@ -45,7 +45,7 @@ class twitter:
         length -= 1
 
       if length > 5:
-        self.bot.msg(buffer.to, "%d messages.... really?! Asshole." % length)
+        sock.msg(buffer.to, "%d messages.... really?! Asshole." % length)
         return
 
       for i, msg in enumerate(split_msg):
@@ -57,19 +57,19 @@ class twitter:
           try:
             tweet = self.api.update_status("%d/%d %s" % (i + 1, length, msg))
           except Exception as e:
-            self.bot.msg(buffer.to, buff.to, "%s" % e)
-          self.bot.msg(buffer.to, "Tweet URL: https://twitter.com/%s/status/%s" % (tweet.author.screen_name, tweet.id))
+            sock.msg(buffer.to, buff.to, "%s" % e)
+          sock.msg(buffer.to, "Tweet URL: https://twitter.com/%s/status/%s" % (tweet.author.screen_name, tweet.id))
 
-  def follow(self, buffer):
+  def follow(self, bot, sock, buffer):
     username   = buffer.msg.split()[1] 
 
     try:
       self.api.create_friendship(username)
-      self.bot.msg(buffer.to, "Followed %s" % username)
+      sock.msg(buffer.to, "Followed %s" % username)
     except Exception as e:
-      self.bot.msg(buffer.to, "Error: %s" % str(e))
+      sock.msg(buffer.to, "Error: %s" % str(e))
 
-  def reply(self, buffer):
+  def reply(self, bot, sock, buffer):
     url        = buffer.msg.split()[1]
     tweet      = ' '.join(buffer.msg.split()[2:])
 
@@ -77,25 +77,25 @@ class twitter:
     if author not in tweet:
       tweet = '@' + author + ' ' + tweet
     tweet = self.api.update_status(tweet, tweet_id)
-    self.bot.msg(buffer.to, "Tweet URL: https://twitter.com/%s/status/%s" % (tweet.author.screen_name, tweet.id))
+    sock.msg(buffer.to, "Tweet URL: https://twitter.com/%s/status/%s" % (tweet.author.screen_name, tweet.id))
 
-  def retweet(self, buffer):
+  def retweet(self, bot, sock, buffer):
     tweet_id = re.search("https?://w?w?w?\.?twitter.com/.*/statuse?s?/([^ ]+)", buffer.msg).group(1)
     self.api.retweet(tweet_id)
     h = HTMLParser.HTMLParser()
     tweet = self.api.get_status(id=tweet_id)
     tweet.text = h.unescape(tweet.text)
-    self.bot.msg(buffer.to, "Retweeted: <%s> %s -- %s" % (tweet.author.screen_name, tweet.text, "https://twitter.com/%s/status/%s" % (tweet.author.screen_name, tweet.id)))
+    sock.msg(buffer.to, "Retweeted: <%s> %s -- %s" % (tweet.author.screen_name, tweet.text, "https://twitter.com/%s/status/%s" % (tweet.author.screen_name, tweet.id)))
 
-  def delete(self, buffer):
+  def delete(self, bot, sock, buffer):
     tweet_id = re.search("https?://w?w?w?\.?twitter.com/.*/statuse?s?/([^ ]+)", buffer.msg).group(1)
     tweet = self.api.destroy_status(id=tweet_id)
-    self.bot.msg(buffer.to, "Deleted: %s" % ("https://twitter.com/%s/status/%s" % (tweet.author.screen_name, tweet.id)))
+    sock.msg(buffer.to, "Deleted: %s" % ("https://twitter.com/%s/status/%s" % (tweet.author.screen_name, tweet.id)))
 
-  def help(self, buffer):
-    self.bot.msg(buffer.to, "Usage:")
-    self.bot.msg(buffer.to, "  twitter.tweet   <tweet>")
-    self.bot.msg(buffer.to, "  twitter.retweet <url>")
-    self.bot.msg(buffer.to, "  twitter.delete  <url>")
-    self.bot.msg(buffer.to, "  twitter.reply   <url>   <tweet>")
-    self.bot.msg(buffer.to, "  twitter.follow  <user>")
+  def help(self, bot, sock, buffer):
+    sock.msg(buffer.to, "Usage:")
+    sock.msg(buffer.to, "  twitter.tweet   <tweet>")
+    sock.msg(buffer.to, "  twitter.retweet <url>")
+    sock.msg(buffer.to, "  twitter.delete  <url>")
+    sock.msg(buffer.to, "  twitter.reply   <url>   <tweet>")
+    sock.msg(buffer.to, "  twitter.follow  <user>")
