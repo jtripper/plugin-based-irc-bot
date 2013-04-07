24e1078376cc1e0b25687df80c8cb98aa54a8b9c
jtripper <jack@jtripper.net>
2013-04-07 13:29:11 +0000

    bad commit

--- fix_plugins.sh
+++ fix_plugins.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 for file in $(ls plugins/*.py)
 do
-  sed -i \'s/self, bot/self/g\' plugins/$file
-  sed -i \'s/self\.bot = bot//g\' plugins/$file 
-  sed -i \'s/self.bot/sock/g\' plugins/$file
-  sed -i \'s/self, buffer/self, bot, sock, buffer/g\' plugins/$file
-  sed -i \'s/self, auth_level, buffer/self, bot, sock, auth_level, buffer/g\' plugins/$file
+  sed -i 's/self, bot/self/g' $file
+  sed -i 's/self\.bot = bot//g' $file 
+  sed -i 's/self.bot/sock/g' $file
+  sed -i 's/self, buffer/self, bot, sock, buffer/g' $file
+  sed -i 's/self, auth_level, buffer/self, bot, sock, auth_level, buffer/g' $file
 done
+
